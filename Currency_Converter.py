93f4a05f83171e3b60ff86216b9bb0d730e9e56f
barrypearseburke <barry.burke3@mydit.ie>
2015-02-10 13:42:10 +0000

    Just upload and class defined

---
+++ Currency_Converter.py
@@ -0,0 +1,20 @@
+__author__ = 'Barry'
+""" A Simple project that will ask the user how much he wants to convert. From What and to what.
+Branch idea in the future is to use pygame and make the  program to use a graphical user interface
+Program will query google currency converter and display value to the user in real time
+
+Author: Barry Burke
+Date started 10th of Febuary 2015
+GNU V2 Public licencse applys
+
+"""
+
+
+class Currency():
+    def __init__(self,Amount,Currency_Have ,CurrencyWant):
+        pass
+    def __str__(self):
+        pass
+
+
+
